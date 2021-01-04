2d49076b500aa3754fc0a0b01b44f5d86a6a3537
Arman Kolahan <akolahan@gmail.com>
2021-01-04 21:07:16 +0100

    profile state added

--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {useSelector} from "react-redux";
 import profileSelectors from "../../../redux/profile/profileSelectors";
+import './Profile.scss'
 
 const Profile: React.FC = () => {
+    const fullName = useSelector(profileSelectors.getFullName);
     const description = useSelector(profileSelectors.getDescription);
     return (
-        <div>
+        <div className="profile">
+            <h3>{fullName}</h3>
             {description}
         </div>
     );
